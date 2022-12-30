2126ae577390f11dc5d7f37986fd2c30ab34015b
Amir Garipov <amirgaripov@Amirs-MacBook-Pro.local>
2022-12-30 16:01:39 +0300

    Added files an

--- LiaAlienolio/settings.py
+++ LiaAlienolio/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'gallery/static'),
+    os.path.join(BASE_DIR, '/gallery/static'),
 )
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
