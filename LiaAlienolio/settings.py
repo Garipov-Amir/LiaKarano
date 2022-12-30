ab6928872605fe6cfdd4342311cf961f37bd4b38
Amir Garipov <amirgaripov@Amirs-MacBook-Pro.local>
2022-12-30 17:04:50 +0300

    Added files an

--- LiaAlienolio/settings.py
+++ LiaAlienolio/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'gallery.apps.GalleryConfig',
     'django_cleanup.apps.CleanupConfig',
