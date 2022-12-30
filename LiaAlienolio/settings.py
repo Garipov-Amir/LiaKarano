50d5c4610d970f24b727b5d95c33a8fb6d8ab669
Amir Garipov <amirgaripov@Amirs-MacBook-Pro.local>
2022-12-30 15:51:40 +0300

    Added files an

--- LiaAlienolio/settings.py
+++ LiaAlienolio/settings.py
@@ -147,7 +147,8 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # CSRF_COOKIE_SECURE = True
 # SECURE_SSL_REDIRECT = True
