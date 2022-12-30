90a4a530db76c068e2ed6c750ea94e2ea9f2923a
Amir Garipov <amirgaripov@Amirs-MacBook-Pro.local>
2022-12-30 16:17:26 +0300

    Added files an

--- LiaAlienolio/settings.py
+++ LiaAlienolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
-ALLOWED_HOSTS = ['liakarano-portfolio.up.railway.app', '0.0.0.0:$PORT', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['liakarano-portfolio.up.railway.app', '0.0.0.0:$PORT', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://liakarano-portfolio.up.railway.app/']
 
