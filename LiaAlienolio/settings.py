0decab6bd30dc3319b21392500b43de99be45b7c
Amir Garipov <amirgaripov@Amirs-MacBook-Pro.local>
2022-12-30 15:56:04 +0300

    Added files an

--- LiaAlienolio/settings.py
+++ LiaAlienolio/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = Falsr
 SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = 'fejhfefkwhflkefhwehflewfhew'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
-ALLOWED_HOSTS = ['liakarano-portfolio.up.railway.app', '0.0.0.0:$PORT']
+ALLOWED_HOSTS = ['liakarano-portfolio.up.railway.app', '0.0.0.0:$PORT', '127.0.0.1']
 
 # CSRF_TRUSTED_ORIGINS = ['https://liakarano-portfolio.up.railway.app/']
 
